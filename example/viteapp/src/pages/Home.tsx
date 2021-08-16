4c0ec4e371a48d7c7b4b312cf4e8141377375b78
Son Hong Do <sondh0127@gmail.com>
2021-08-16 18:47:36 +0700

    feat: resolve assets source on dev

--- example/viteapp/src/pages/Home.tsx
+++ example/viteapp/src/pages/Home.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function () {
   return (
-    <h2 className="app-nav-item" style={{ borderColor: "red" }}>
-      Homeddd
-    </h2>
+    <div>
+      <img alt="Vite logo" src="/src/logo.svg" style={{ width: 300 }}/>
+      <h2 className="app-nav-item" style={{ borderColor: "red" }}>
+        Homeddd
+      </h2>
+    </div>
   );
 }
