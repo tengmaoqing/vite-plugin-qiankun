9fb2fc829885e7632e9c6c81372328a865d29b3c
ext.zhuyunfeng1 <i@zhuyunfeng.com>
2023-01-06 13:50:19 +0800

    chore: modify for jd mfe

--- src/helper.ts
+++ src/helper.ts
@@ -12,7 +12,7 @@ export type QiankunLifeCycle = {
 
 export interface QiankunWindow {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  __POWERED_BY_QIANKUN__?: boolean;
+  __POWERED_BY_MFE__?: boolean;
   [x: string]: any
 }
 
@@ -20,7 +20,7 @@ export const qiankunWindow: QiankunWindow = typeof window !== 'undefined' ? (win
 
 export const renderWithQiankun = (qiankunLifeCycle: QiankunLifeCycle) => {
   // 函数只有一次执行机会，需要把生命周期赋值给全局
-  if (qiankunWindow?.__POWERED_BY_QIANKUN__) {
+  if (qiankunWindow?.__POWERED_BY_MFE__) {
     if (!window.moudleQiankunAppLifeCycles) {
       window.moudleQiankunAppLifeCycles = {}
     }
